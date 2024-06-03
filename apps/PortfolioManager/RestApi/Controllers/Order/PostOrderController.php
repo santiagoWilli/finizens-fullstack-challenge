f01d3e87e41f933499688c4b69317bce44bf9728
santiagoWilli <santiago30mw@gmail.com>
2024-06-03 20:02:23 +0200

    imp: if portfolio not found, respond with a 404. One red test for boy_post.feature

--- apps/PortfolioManager/RestApi/Controllers/Order/PostOrderController.php
+++ apps/PortfolioManager/RestApi/Controllers/Order/PostOrderController.php
@@ -3,16 +3,27 @@
 namespace Finizens\Apps\PortfolioManager\RestAPI\Controllers\Order;
 
 use Finizens\PortfolioManagement\Order\Application\CreateOrder;
+use Finizens\PortfolioManagement\Portfolio\Application\FindPortfolio;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 final class PostOrderController
 {
-    public function __construct(private readonly CreateOrder $createOrder) {}
+    public function __construct(
+        private readonly CreateOrder $createOrder,
+        private readonly FindPortfolio $findPortfolio
+    ) {}
 
     public function __invoke(Request $request, Response $response): Response
     {
         $body = $request->getParsedBody();
+        if (!isset($body['id'], $body['portfolio'], $body['allocation'], $body['type'], $body['shares'])) {
+            return $response
+                ->withStatus(400)
+                ->withHeader('Content-Type', 'application/json');
+        }
+
+        ($this->findPortfolio)($body['portfolio']);
         ($this->createOrder)($body['id'], $body['portfolio'], $body['allocation'], $body['type'], $body['shares']);
 
         return $response
